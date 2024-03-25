be494a80b61814ecee6094a222660d76b2346e88
sclement2 <sclement2@gmail.com>
2024-03-25 17:52:43 +0000

    Delegate selection and choice to display to separate methods. Add better prompts

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 
 namespace dotnetcore
 {
@@ -7,9 +6,11 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            ChestChallenge();
+            //ChestChallenge();
+            SoupChallenge();
         }
 
+        // Simula's Test Challenge
         private static void ChestChallenge()
         {
             ChestState state = ChestState.Open;
@@ -52,7 +53,140 @@ namespace dotnetcore
                 iteration++;
                 if (iteration >= maxIteration || action == ChestActions.Exit)
                     break;
+            }
+        }
 
+        // Simula's Soup Challenge
+        private static void SoupChallenge()
+        {
+            (Seasonings seasoning, MainIngredients mainIngredient, Recipes recipe) meal;
+            int iteration = 0;
+            int maxIteration = 1;
+
+            Console.WriteLine("Let's make a special meal just for you. ");
+            Console.Write("I have many different meals to choose from. ");
+
+            while (true)
+            {
+
+                Console.WriteLine("In order to make your meal I need to know 3 things.");
+                Console.WriteLine("(1) - Type of seasoning you want");
+                Console.WriteLine("(2) - Main ingredient you want");
+                Console.WriteLine("(3) - Recipe you want");
+
+                meal.seasoning = GetEnumSelection<Seasonings>("seasoning");
+                meal.mainIngredient = GetEnumSelection<MainIngredients>("main ingredient");
+                meal.recipe = GetEnumSelection<Recipes>("recipe");
+
+                /*
+                while (true)
+                {
+                    Console.WriteLine($"\nTell me what seasoning you would like. ");
+                    Console.Write("Your choices are as follows: ");
+                    Console.Write(GetEnumAsString(typeof(Seasonings)));
+
+                    string seasoningResponse = Console.ReadLine();
+                    if (Enum.TryParse(seasoningResponse, ignoreCase: true, out Seasonings parsedSeasoning))
+                    {
+                        meal.seasoning = parsedSeasoning;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, that's not a valid option. Let's try this again.");
+                        continue;
+                    }
+                }
+
+
+                while (true)
+                {
+                    Console.WriteLine($"\nTell me what main ingedient you would like. ");
+                    Console.Write("Your choices are as follows: ");
+                    Console.Write(GetEnumAsString(typeof(MainIngredients)));
+
+                    string mainIngredientResponse = Console.ReadLine();
+                    if (Enum.TryParse(mainIngredientResponse, ignoreCase: true, out MainIngredients parsedMainIngredient))
+                    {
+                        meal.mainIngredient = parsedMainIngredient;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, that's not a valid option. Let's try this again.");
+                        continue;
+                    }
+                }
+
+                while (true)
+                {
+                    Console.WriteLine($"\nTell me what recipe you would like. ");
+                    Console.Write("Your choices are as follows: ");
+                    Console.Write(GetEnumAsString(typeof(Recipes)));
+
+                    string recipeResponse = Console.ReadLine();
+                    if (Enum.TryParse(recipeResponse, ignoreCase: true, out Recipes parsedRecipe))
+                    {
+                        meal.recipe = parsedRecipe;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, that's not a valid option. Let's try this again.");
+                        continue;
+                    }
+                }
+                */
+
+                Console.Write("\nHere's your special meal - ");
+                Console.Write($"{meal.seasoning} {meal.mainIngredient} {meal.recipe}\n");
+                iteration++;
+                if (iteration == maxIteration) break;
+            }
+        }
+
+        private static string GetEnumAsString(Type enumType)
+        {
+            Array enumValues = Enum.GetValues(enumType);
+            string enumString = "";
+            for (int i = 0; i < enumValues.Length - 1; i++)
+            {
+                enumString += enumValues.GetValue(i).ToString() + ", ";
+            }
+            enumString += enumValues.GetValue(enumValues.Length - 1).ToString() + ": ";
+            return enumString;
+        }
+
+        private static TEnum GetEnumSelection<TEnum>(string attributeName) where TEnum : struct, Enum
+        {
+            int tries = 1;
+            int maxTries = 2;
+            int randomIndex = 0;
+            while (true)
+            {
+                Console.WriteLine($"\nTell me what {attributeName} you would like. ");
+                Console.Write("Your choices are as follows: ");
+                Console.Write(GetEnumAsString(typeof(TEnum)));
+
+                string response = Console.ReadLine();
+                if (Enum.TryParse<TEnum>(response, ignoreCase: true, out TEnum parsedValue))
+                {
+                    return parsedValue;
+                }
+                else if (tries > maxTries)
+                {
+                    Array enumValues = Enum.GetValues(typeof(TEnum));
+                    Console.WriteLine($"OK, this does not seem to be working. I will choose a {attributeName} for you.");
+                    TEnum randomChoice = (TEnum)enumValues.GetValue(randomIndex);
+                    Console.WriteLine($"Your {attributeName} is {randomChoice}");
+                    return randomChoice;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, that's not a valid option. Let's try this again.");
+                    tries++;
+                    continue;
+                }
             }
         }
 
@@ -70,6 +204,28 @@ namespace dotnetcore
             Lock,
             Unlock,
             Exit
+        }
+
+        enum Recipes
+        {
+            Soup,
+            Stew,
+            Gumbo
+        }
+
+        enum MainIngredients
+        {
+            Mushroom,
+            Chicken,
+            Carrot,
+            Potatoe
+        }
+
+        enum Seasonings
+        {
+            Spicy,
+            Salty,
+            Sweet
         }
     }
 }
